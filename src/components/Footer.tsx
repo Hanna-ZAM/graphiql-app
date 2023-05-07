ccca7a9781635ed27933da2c4ac0775a16901979
Chizhov Maxim <chizhov98ip@gmail.com>
2023-05-07 13:58:13 +0300

    feat:add footer with styles,create footer link component,add footer in root layout

---
+++ src/components/Footer.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import RssLogo from '../assets/rs_school.svg';
+import FooterLink from './FooterLink';
+
+const Footer = () => {
+  const TEAMMATES_GITHUB = [
+    {
+      id: 1,
+      link: 'https://github.com/kanoplich'
+    },
+    {
+      id: 2,
+      link: 'https://github.com/hanna-zam'
+    },
+    {
+      id: 3,
+      link: 'https://github.com/chizhovmn'
+    }
+  ];
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="team">
+          {TEAMMATES_GITHUB.map((person) => (
+            <FooterLink link={person.link} key={person.id} />
+          ))}
+        </div>
+        2023
+        <RssLogo className={'footer-icon rss-school'} />
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
