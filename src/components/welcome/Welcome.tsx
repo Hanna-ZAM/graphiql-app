6dc8f5aaf5bbae89976d1af052356682f48d17a7
Chizhov Maxim <chizhov98ip@gmail.com>
2023-05-08 16:03:03 +0300

    feat:add translations to header ,welcome and main page serverrender

--- src/components/welcome/Welcome.tsx
+++ src/components/welcome/Welcome.tsx
@@ -7,15 +7,8 @@ import { useRouter } from 'next/router';
 const Welcome = () => {
   const { t } = useTranslation('welcome');
   const router = useRouter();
-  // TODO:Delete exmaple translate buttons
   return (
     <section className="welcome">
-      <Link href={router.asPath} locale={'en'}>
-        en
-      </Link>
-      <Link href={router.asPath} locale={'ru'}>
-        ru
-      </Link>
       <h2 className="welcome-header">{t('welcome_header')}</h2>
       <hr />
       <div className="welcome-main container">
--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,5 +1,15 @@
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import React from 'react';
 
 export default function MainPage() {
   return <>Main page</>;
+}
+
+export async function getStaticProps({ locale }: { locale: string }) {
+  const lang = locale ?? 'en';
+  return {
+    props: {
+      ...(await serverSideTranslations(lang, ['common', 'header']))
+    }
+  };
 }
