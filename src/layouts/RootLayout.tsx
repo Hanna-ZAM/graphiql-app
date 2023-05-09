bb04cb56bb0bc0670b55f40fb95953d7310891ac
Chizhov Maxim <chizhov98ip@gmail.com>
2023-05-09 12:04:54 +0300

    feat:add auth context to redirect on welcome page if user not auth,refactor code

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { createContext, useEffect, useMemo, useState } from 'react';
 import Header from '@/components/header/Header';
 import Footer from '@/components/footer/Footer';
+import { AuthContextType } from '@/types';
+import { useRouter } from 'next/router';
+
+export const AuthContext = createContext<AuthContextType | null>(null);
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const router = useRouter();
+  const AuthMemo = useMemo(() => ({ isLogin, setIsLogin }), [isLogin]);
+  useEffect(() => {
+    const isLoggedIn = sessionStorage.getItem('isLoggedIn');
+    if (isLoggedIn) {
+      setIsLogin(true);
+    }
+  }, []);
+  useEffect(() => {
+    const isLoggedIn = sessionStorage.getItem('isLoggedIn');
+    if (isLoggedIn) return;
+    router.replace('/');
+  }, [router]);
   return (
-    <>
+    <AuthContext.Provider value={AuthMemo}>
       <Header />
       <main className="main container">{children}</main>
       <Footer />
-    </>
+    </AuthContext.Provider>
   );
 };
 
