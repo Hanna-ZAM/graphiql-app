48797a3caab7b58ea2e7735e3500dff116d327fc
Andrei Kanoplich <andrei.kanoplich@gmail.com>
2023-05-16 23:49:03 +0300

    feat: add graphql-request

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -4,14 +4,35 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Button, Tooltip } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+import { request, gql } from 'graphql-request';
+
+const URL = 'https://rickandmortyapi.graphcdn.app/';
 
 const Editor = () => {
   const { t } = useTranslation('common');
+  const [editValue, setEditValue] = useState('');
+
+  const handleChange = (e: string) => {
+    setEditValue(e);
+  };
+
+  const handleClick = () => {
+    const query = gql`
+      ${editValue}
+    `;
+
+    request(URL, query).then((data) => console.log(data));
+  };
 
   return (
     <div className="editor__wrapper">
       <Tooltip placement="bottom" title={t('query')}>
-        <FontAwesomeIcon icon={faCirclePlay} className="editor__svg" />
+        <FontAwesomeIcon
+          icon={faCirclePlay}
+          className="editor__svg"
+          onClick={handleClick}
+        />
       </Tooltip>
       <div className="editor__button">
         <Button danger>Docs</Button>
@@ -21,6 +42,7 @@ const Editor = () => {
         className="editor__code"
         height="100%"
         extensions={[javascript({ jsx: true })]}
+        onChange={(e) => handleChange(e)}
       />
     </div>
   );
