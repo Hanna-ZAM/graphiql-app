1729bec9cd9032e4fe50d605e7753809aeba7563
Hanna Zalatarevich <a.zoloto@mail.ru>
2023-05-28 18:53:30 +0300

    fix: rename files in header folder

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
-import Logo from './Logo13';
-import Navigate from './Navigate123';
+import Logo from './Logo';
+import Navigate from './Navigate';
 import { useEffect, useRef } from 'react';
 
 const Header = () => {
